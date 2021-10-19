c1cb76fd16aa3880970a23297212e8fca69385bd
andrew-weisman <44007191+andrew-weisman@users.noreply.github.com>
2021-10-19 18:24:46 -0400

    Update load_boost_env.sh to what it was supposed to be

--- load_boost_env.sh
+++ load_boost_env.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
-
-# BOOST_HOME="/usr/local/boost/1.76_gcc-4.8.5"
-# BOOSTLIB="/usr/local/boost/1.76_gcc-4.8.5/lib"
-# BOOSTINC="/usr/local/boost/1.76_gcc-4.8.5/include"
-# setenv("BOOSTLIB","$BOOSTLIB")
-# prepend_path("LIBRARY_PATH","$BOOSTLIB")
-# prepend_path("LD_LIBRARY_PATH","$BOOSTLIB")
-# prepend_path("LD_RUN_PATH","$BOOSTLIB")
-# prepend_path("CMAKE_LIBRARY_PATH","$BOOSTLIB")
-# prepend_path("CMAKE_INCLUDE_PATH","$BOOSTINC")
-# prepend_path("CPATH","$BOOSTINC")
-
-BOOST_HOME="/home/weismanal/notebook/2021-09-08/installing_memsurfer_take_2/MemSurfer/external"
+BOOST_HOME="$MEMSURFER_INSTALL/NCI-DOE-Collab-Pilot2-MemSurfer/external"
 BOOSTLIB="$BOOST_HOME/lib"
 BOOSTINC="$BOOST_HOME/include"
 export BOOSTLIB=$BOOSTLIB
